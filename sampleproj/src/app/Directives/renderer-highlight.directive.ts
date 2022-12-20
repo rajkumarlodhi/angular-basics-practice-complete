422a2f26aa0c72e0e0133b0280789b33bb66c683
rajkumarlodhi <49247880+rajkumarlodhi@users.noreply.github.com>
2022-12-20 18:01:40 +0530

    @HostListener with directive to listen events
    @HostListener is nothing but event listener like mouseenter, mousemove, mouseleave, click, etc

--- sampleproj/src/app/Directives/renderer-highlight.directive.ts
+++ sampleproj/src/app/Directives/renderer-highlight.directive.ts
@@ -1,4 +1,4 @@
-import { Directive, ElementRef, OnInit, Renderer2 } from '@angular/core';
+import { Directive, ElementRef, HostListener, OnInit, Renderer2 } from '@angular/core';
 
 @Directive({
   selector: '[appRendererHighlight]'
@@ -8,7 +8,19 @@ export class RendererHighlightDirective implements OnInit {
   constructor(private element: ElementRef, private renderer: Renderer2) { }
 
   ngOnInit(): void {
+    // this.renderer.setStyle(this.element.nativeElement, 'background-color', 'green');
+  }
+  @HostListener('click') OnClick() {
     this.renderer.setStyle(this.element.nativeElement, 'background-color', 'green');
+  }
+  @HostListener('onmouseenter') OnMouseEnter() {
+    this.renderer.setStyle(this.element.nativeElement, 'background-color', 'yellow');
+  }
+  @HostListener('onmouseleave') OnMouseLeave() {
+    this.renderer.setStyle(this.element.nativeElement, 'background-color', 'gray');
+  }
+  @HostListener('onmousemove') onMouseMove() {
+    this.renderer.setStyle(this.element.nativeElement, 'background-color', 'gray');
   }
 
 }
