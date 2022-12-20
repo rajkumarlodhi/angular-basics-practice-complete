3728f53deb45d13f746b84f32153136ccef10dd7
rajkumarlodhi <49247880+rajkumarlodhi@users.noreply.github.com>
2022-12-20 18:17:25 +0530

    using @HostBinding to set value of color
    It is easy to use rather than renderer2

--- sampleproj/src/app/Directives/renderer-highlight.directive.ts
+++ sampleproj/src/app/Directives/renderer-highlight.directive.ts
@@ -1,26 +1,30 @@
-import { Directive, ElementRef, HostListener, OnInit, Renderer2 } from '@angular/core';
+import { Directive, ElementRef, HostBinding, HostListener, OnInit, Renderer2 } from '@angular/core';
 
 @Directive({
   selector: '[appRendererHighlight]'
 })
 export class RendererHighlightDirective implements OnInit {
-
+  @HostBinding('style.backgroundColor') color: string = '';
   constructor(private element: ElementRef, private renderer: Renderer2) { }
 
   ngOnInit(): void {
     // this.renderer.setStyle(this.element.nativeElement, 'background-color', 'green');
   }
+  //using renderer2
+  // @HostListener('click') OnClick() {
+  //   this.renderer.setStyle(this.element.nativeElement, 'background-color', 'green');
+  // }
   @HostListener('click') OnClick() {
-    this.renderer.setStyle(this.element.nativeElement, 'background-color', 'green');
+    this.color = 'green'
   }
   @HostListener('onmouseenter') OnMouseEnter() {
-    this.renderer.setStyle(this.element.nativeElement, 'background-color', 'yellow');
+    this.color = 'green'
   }
   @HostListener('onmouseleave') OnMouseLeave() {
-    this.renderer.setStyle(this.element.nativeElement, 'background-color', 'gray');
+    this.color = 'yellow'
   }
   @HostListener('onmousemove') onMouseMove() {
-    this.renderer.setStyle(this.element.nativeElement, 'background-color', 'gray');
+    this.color = 'gray'
   }
 
 }
