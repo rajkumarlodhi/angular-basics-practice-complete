b0a0e591159a7949a149f8701febe6bc5813ad6b
rajkumarlodhi <49247880+rajkumarlodhi@users.noreply.github.com>
2022-12-21 15:40:02 +0530

    update custom structural directive

--- sampleproj/src/app/Directives/UlternateIf.directive.ts
+++ sampleproj/src/app/Directives/UlternateIf.directive.ts
@@ -1,18 +1,27 @@
 import { Directive, Input, OnChanges, TemplateRef, ViewContainerRef } from "@angular/core";
 
 @Directive({
-    selector: '[appUlternateIf]'
+    selector: '[appUlternateIf]',
 })
 
 export class appUlternateIfDirective implements OnChanges {
-    @Input() appUlternateIf = 'true';
 
-    ngOnChanges() {
-        if (this.appUlternateIf) {
+    @Input() set appUlternateIf(condition: boolean) {
+        if (condition) {
             this.vcRef.createEmbeddedView(this.templateRef)
         } else {
             this.vcRef.clear();
         }
+    }
+
+    // @Input() appUlternateIf: boolean = true;
+
+    ngOnChanges() {
+        // if (this.appUlternateIf) {
+        //     this.vcRef.createEmbeddedView(this.templateRef)
+        // } else {
+        //     this.vcRef.clear();
+        // }
     }
     constructor(private templateRef: TemplateRef<any>, private vcRef: ViewContainerRef) { }
 
