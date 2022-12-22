b24c7d51fee1511dcb464fb00372b7ca59a00168
rajkumarlodhi <49247880+rajkumarlodhi@users.noreply.github.com>
2022-12-22 22:46:08 +0530

    create simple routing (not single page application routing)
    create simple routing (not single page application routing because it is refreshing)

--- angularrouting/src/app/app.module.ts
+++ angularrouting/src/app/app.module.ts
@@ -2,13 +2,27 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
+import { HomeComponent } from './home/home.component';
+import { UsersComponent } from './users/users.component';
+import { CategoriesComponent } from './categories/categories.component';
+import { Routes, RouterModule } from '@angular/router';
+
+const appRoutes: Routes = [
+  { path: '', component: HomeComponent },
+  { path: 'users', component: UsersComponent },
+  { path: 'categories', component: CategoriesComponent },
+];
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HomeComponent,
+    UsersComponent,
+    CategoriesComponent,
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    RouterModule.forRoot(appRoutes),
   ],
   providers: [],
   bootstrap: [AppComponent]
