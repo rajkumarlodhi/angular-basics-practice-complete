30638c17e376c8e656340d02a51a586b499bd7fc
rajkumarlodhi <49247880+rajkumarlodhi@users.noreply.github.com>
2022-12-23 22:48:34 +0530

    Separate all the Routing configuration code into another file app-routing.module
    Separate all the Routing configuration code into another file app-routing.module in the angular.

--- angularrouting/src/app/app.module.ts
+++ angularrouting/src/app/app.module.ts
@@ -5,22 +5,10 @@ import { AppComponent } from './app.component';
 import { HomeComponent } from './home/home.component';
 import { UsersComponent } from './users/users.component';
 import { CategoriesComponent } from './categories/categories.component';
-import { Routes, RouterModule } from '@angular/router';
+import { AppRoutingModule } from './app-routing.module';
 import { UserComponent } from './user/user.component';
 import { EditUserComponent } from './edit-user/edit-user.component';
 import { PageNotFoundComponent } from './page-not-found/page-not-found.component';
-
-const appRoutes: Routes = [
-  { path: '', component: HomeComponent },
-  {
-    path: 'users', component: UsersComponent, children: [
-      { path: ':id/:name', component: UserComponent },
-      { path: ':id/:name/edit', component: EditUserComponent }]
-  },
-  { path: 'categories', component: CategoriesComponent },
-  { path: 'not-found', component: PageNotFoundComponent },
-  { path: '**', redirectTo: 'not-found' },
-];
 
 @NgModule({
   declarations: [
@@ -34,7 +22,7 @@ const appRoutes: Routes = [
   ],
   imports: [
     BrowserModule,
-    RouterModule.forRoot(appRoutes),
+    AppRoutingModule,
   ],
   providers: [],
   bootstrap: [AppComponent]
