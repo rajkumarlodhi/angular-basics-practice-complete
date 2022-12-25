dbad45fd6ba9fcd81d79fae2acee67805bfa5c57
rajkumarlodhi <49247880+rajkumarlodhi@users.noreply.github.com>
2022-12-25 18:46:24 +0530

    the core of the Observables in rxjs. Need of subscribe & unsubscribe
    Understand the core of the Observables in rxjs. Need of subscribe & unsubscribe in the angular.

--- angularrouting/src/app/home/home.component.ts
+++ angularrouting/src/app/home/home.component.ts
@@ -1,19 +1,26 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, OnDestroy } from '@angular/core';
 import { ActivatedRoute, Params } from '@angular/router';
-
+import { Subscription, interval } from 'rxjs';
 @Component({
   selector: 'app-home',
   templateUrl: './home.component.html',
   styleUrls: ['./home.component.css']
 })
-export class HomeComponent implements OnInit {
-
+export class HomeComponent implements OnInit, OnDestroy {
+  //intervalSubscription: Subscription = new Subscription;
+  routeSubscription: Subscription = new Subscription;
   constructor(private route: ActivatedRoute) { }
-
   ngOnInit(): void {
     this.route.data.subscribe((data: Params) => {
       console.log(data, 'data+++++');
     })
+
+    this.routeSubscription = interval(1000).subscribe(count => {
+      console.log(count);
+    })
+  }
+  ngOnDestroy(): void {
+    this.routeSubscription.unsubscribe();
   }
 
 }
