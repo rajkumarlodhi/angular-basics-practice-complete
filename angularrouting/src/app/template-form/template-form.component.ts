deea13cf25747dea5a0175a7f14071369c14cbca
rajkumarlodhi <49247880+rajkumarlodhi@users.noreply.github.com>
2022-12-26 12:24:15 +0530

    Use ViewChild in the Template Driven Form to get Form Object
    Advantages of using ViewChild in the Template Driven Form to get Form Object in angular.

--- angularrouting/src/app/template-form/template-form.component.ts
+++ angularrouting/src/app/template-form/template-form.component.ts
@@ -1,4 +1,5 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, ViewChild } from '@angular/core';
+import { NgForm } from '@angular/forms';
 
 @Component({
   selector: 'app-template-form',
@@ -6,13 +7,17 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./template-form.component.css']
 })
 export class TemplateFormComponent implements OnInit {
+  @ViewChild('f') signUpForm = NgForm;
 
   constructor() { }
 
   ngOnInit(): void {
   }
-  onFormSubmit(form: Object) {
-    console.log(form, 'form data++++')
+  onFormSubmit() {
+    console.log(this.signUpForm, 'form data++++')
+  }
+  checkData() {
+    console.log(this.signUpForm, 'chekc data changes');
   }
 
 }
