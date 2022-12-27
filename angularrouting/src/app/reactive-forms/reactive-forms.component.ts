8dabca3e0c82b4b3a08b3784207fb7700438ed53
rajkumarlodhi <49247880+rajkumarlodhi@users.noreply.github.com>
2022-12-27 22:31:49 +0530

    Dynamically Add Form Controls with FormArray, FormArrayName in the Reactive Forms
    Dynamically Add Form Controls with FormArray FormArrayName in the Reactive Forms - Angular.

--- angularrouting/src/app/reactive-forms/reactive-forms.component.ts
+++ angularrouting/src/app/reactive-forms/reactive-forms.component.ts
@@ -1,5 +1,5 @@
 import { Component, OnInit } from '@angular/core';
-import { FormControl, FormGroup, Validators } from '@angular/forms';
+import { FormArray, FormControl, FormGroup, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-reactive-forms',
@@ -13,8 +13,13 @@ export class ReactiveFormsComponent implements OnInit {
   //   'username': new FormControl('', Validators.required),
   //   'email': new FormControl('', [Validators.required, Validators.email]),
   //   'gender': new FormControl('female'),
+  //   'hobbies': new FormArray([])
   // });
   constructor() { }
+
+  get hobbyControls() {
+    return (<FormArray>this.signUpForm.get('hobbies')).controls;
+  }
 
   ngOnInit(): void {
     this.signUpForm = new FormGroup({
@@ -23,11 +28,17 @@ export class ReactiveFormsComponent implements OnInit {
         'email': new FormControl(null, [Validators.required, Validators.email]),
       }),
       'gender': new FormControl('female'),
+      'hobbies': new FormArray([])
     })
   }
   onSubmit() {
     console.log(this.signUpForm, 'reactive signup form data');
     console.warn(this.signUpForm.value);
+  }
+  onAddHobby() {
+    const control = new FormControl(null, [Validators.required]);
+    (<FormArray>this.signUpForm.get('hobbies')).push(control);
+    console.log(this.signUpForm);
   }
 
 }
