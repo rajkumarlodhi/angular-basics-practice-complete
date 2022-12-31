28b59aaabc8665ceee0fe2613a5f21e5527c62af
rajkumarlodhi <49247880+rajkumarlodhi@users.noreply.github.com>
2022-12-31 21:15:28 +0530

    Accessing Http Response Event Object with Response Interceptors
    Accessing Http Response Event Object with Response Interceptors in the Angular.

--- angularrouting/src/app/services/auth-interceptor.service.ts
+++ angularrouting/src/app/services/auth-interceptor.service.ts
@@ -1,5 +1,5 @@
-import { HttpEvent, HttpHandler, HttpInterceptor, HttpRequest } from "@angular/common/http";
-import { Observable } from "rxjs";
+import { HttpEvent, HttpEventType, HttpHandler, HttpInterceptor, HttpRequest } from "@angular/common/http";
+import { map, Observable, tap } from "rxjs";
 
 export class AuthInterceptorService implements HttpInterceptor {
     intercept(req: HttpRequest<any>, next: HttpHandler): Observable<HttpEvent<any>> {
@@ -8,7 +8,13 @@ export class AuthInterceptorService implements HttpInterceptor {
             headers: req.headers.append('auth', 'abc'),
             params: req.params.append('hai', 'Helo world')
         })
-        return next.handle(modifiedRequest);
+        return next.handle(modifiedRequest).pipe(tap((event) => {
+            console.log(event, 'on interceptor');
+            console.log('Response from interceptor');
+            if (event.type === HttpEventType.Response) {
+                console.log(event.body, 'on boddy');
+            }
+        }));
     }
 
 }
