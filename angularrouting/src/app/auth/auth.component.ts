8d93f64433a78032d34ee4944ca2f1f9a78f1494
rajkumarlodhi <49247880+rajkumarlodhi@users.noreply.github.com>
2023-01-01 17:54:07 +0530

    Apply Template Driven Approach and implement Validations for the authentication form
    Apply Template Driven Approach and implement Validations for the authentication form - Angular

--- angularrouting/src/app/auth/auth.component.ts
+++ angularrouting/src/app/auth/auth.component.ts
@@ -1,4 +1,5 @@
 import { Component } from "@angular/core";
+import { FormControl, NgForm } from "@angular/forms";
 
 @Component({
     selector: 'app-auth',
@@ -6,7 +7,24 @@ import { Component } from "@angular/core";
 })
 export class AuthComponent {
     isLoginMode = true;
+    passwordErrors: any = '';
     onSwitchMode() {
         this.isLoginMode = !this.isLoginMode;
+    }
+    constructor() { }
+    onFormSubmit(authForm: NgForm) {
+        console.log(authForm.value);
+        // console.log(authForm);
+        //console.log(authForm.form.controls['email'].errors?.['required']);
+    }
+    getPasswordErrors(password: any) {
+        console.log(password, 'password');
+        if (!password.valid && password.touched) {
+            this.passwordErrors = 'Password is requiredd';
+        }
+        if (password.errors) {
+            this.passwordErrors = 'Password should be of 6 characters length';
+        }
+
     }
 }
